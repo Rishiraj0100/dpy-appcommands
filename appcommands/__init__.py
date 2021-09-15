d0d0710937a9eab5e7e33d0dc7f2bba5f544efcb
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-15 12:23:49 +0530

    Update __init__.py

--- appcommands/__init__.py
+++ appcommands/__init__.py
@@ -1,6 +1,11 @@
 __author__ = "Rishiraj0100"
 __version__ = "1.0.0"
+import discord
+
+if not discord.__version__.startswith("2.0"):
+  raise RuntimeError(f"This module requires dpy v2.0 not {discord.__version__}")
 
 from .client import *
 from .models import *
 from .enums import *
+
