0544cbc26f3b270fcb4f4d81b84055cd1187a222
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-10-08 12:08:55 +0530

    Update utils.py

--- appcommands/utils.py
+++ appcommands/utils.py
@@ -1,7 +1,24 @@
 import discord
 
+__all__ = (
+    "MISSING",
+   "missing"
+)
 
-MISSING = discord.utils.MISSING
+class _MissingSentinel:
+    def __eq__(self, other):
+        return False
+
+    def __bool__(self):
+        return False
+
+    def __repr__(self):
+        return '...'
+
+    def __call__(self):
+        return self
+
+MISSING = _MissingSentiel()
 
 def missing(*args, **kwargs) -> MISSING:
   return MISSING
