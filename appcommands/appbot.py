453cc88d4d387c2493f748ba8ae9860ce60f1ceb
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-12-20 19:28:25 +0530

    Create appbot.py

---
+++ appcommands/appbot.py
@@ -0,0 +1,254 @@
+import appcommands, re, zlib, discord, io, aiohttp, os
+
+from os import environ as env
+from typing import Any, List
+from appcommands import OptionType, Option
+
+
+flags: List[str] = [
+  "jishaku no underscore",
+  "jishaku force paginator",
+  "jishaku no dm traceback",
+  "jishaku retain",
+]
+
+class AppBot(appcommands.Bot):
+  def __init__(self) -> None:
+    super().__init__(
+      command_prefix="$",
+      strip_after_prefix=True,
+      case_insensitive=True
+    )
+
+  async def on_connect(self) -> None:
+    self.session = aiohttp.ClientSession(loop=self.loop)
+    await self.change_presence(
+      activity=discord.Activity(
+        type=discord.ActivityType.watching,
+        name="dpy-appcommands v{v}".format(
+          v=appcommands.__version__
+        )
+      ),
+      status=discord.Status.dnd
+    )
+    print(f"{self.user} started!")
+
+    async def close(self):
+      await super().close()
+      await self.session.close()
+
+
+bot: AppBot = AppBot()
+
+def enable(content: str, t: str = "t") -> None:
+  t: str = content.upper()
+  while " " in t:
+    t: str = t.replace(" ", "_")
+
+  env[t]: str = t
+
+for flag in flags:
+  enable(flag)
+
+def finder(text, collection, *, key=None, lazy=True):
+  suggestions = []
+  text = str(text)
+  pat = ".*?"
+  pat = pat.join(map(re.escape, text))
+  regex = re.compile(pat, flags=re.IGNORECASE)
+  for item in collection:
+    to_search = key(item) if key else item
+    r = regex.search(to_search)
+    if r:
+      suggestions.append((len(r.group()), r.start(), item))
+
+  def sort_key(tup):
+    if key:
+      return tup[0], tup[1], key(tup[2])
+    return tup
+
+  if lazy:
+    return (z for _, _, z in sorted(suggestions, key=sort_key))
+  else:
+    return [z for _, _, z in sorted(suggestions, key=sort_key)]
+
+
+_ = "_"
+Q = Option(_, "query to get documentation", OptionType.STRING, True)
+
+# https://github.com/Rapptz/RoboDanny/blob/rewrite/cogs/api.py
+
+
+class SphinxObjectFileReader:
+  BUFSIZE: int = 16 * 1024
+
+  def __init__(self, buffer):
+    self.stream = io.BytesIO(buffer)
+
+  def readline(self):
+    return self.stream.readline().decode("utf-8")
+
+  def skipline(self):
+    self.stream.readline()
+
+  def read_compressed_chunks(self):
+    decompressor = zlib.decompressobj()
+    while True:
+      chunk = self.stream.read(self.BUFSIZE)
+      if len(chunk) == 0:
+        break
+      yield decompressor.decompress(chunk)
+    yield decompressor.flush()
+
+
+  def read_compressed_lines(self):
+    buf = b""
+    for chunk in self.read_compressed_chunks():
+      buf += chunk
+      pos = buf.find(b"\n")
+      while pos != -1:
+        yield buf[:pos].decode("utf-8")
+        buf = buf[pos + 1 :]
+        pos = buf.find(b"\n")
+
+class RTFMCog(appcommands.Cog):
+  def __init__(self, bot: AppBot):
+    self.bot: AppBot = bot
+
+  def parse_object_inv(self, stream, url):
+    result = {}
+    inv_version = stream.readline().rstrip()
+    if inv_version != "# Sphinx inventory version 2":
+      raise RuntimeError("Invalid objects.inv file version.")
+
+    projname = stream.readline().rstrip()[11:]
+    version = stream.readline().rstrip()[11:]
+    line = stream.readline()
+    if "zlib" not in line:
+      raise RuntimeError("Invalid objects.inv file, not z-lib compatible.")
+
+    entry_regex = re.compile(r"(?x)(.+?)\s+(\S*:\S*)\s+(-?\d+)\s+(\S+)\s+(.*)")
+    for line in stream.read_compressed_lines():
+      match = entry_regex.match(line.rstrip())
+      if not match:
+        continue
+
+      name, directive, prio, location, dispname = match.groups()
+      domain, _, subdirective = directive.partition(":")
+      if directive == "py:module" and name in result:
+        continue
+
+      if directive == "std:doc":
+        subdirective = "label"
+
+      if location.endswith("$"):
+        location = location[:-1] + name
+
+      key = name if dispname == "-" else dispname
+      prefix = f"{subdirective}:" if domain == "std" else ""
+
+      if projname == "discord.py":
+        key = key.replace("discord.ext.commands.", "").replace("discord.", "")
+      elif projname == "dpy-appcommands":
+        key = key.replace("appcommands.", "")
+
+      result[f"{prefix}{key}"] = os.path.join(url, location)
+
+    return result
+
+  async def build_rtfm_lookup_table(self, page_types):
+    cache = {}
+    for key, page in page_types.items():
+      sub = cache[key] = {}
+      async with self.bot.session.get(page + "/objects.inv") as resp:
+        if resp.status != 200:
+          raise RuntimeError("Cannot build rtfm lookup table, try again later.")
+
+        stream = SphinxObjectFileReader(await resp.read())
+        cache[key] = self.parse_object_inv(stream, page)
+
+      self._rtfm_cache = cache
+
+  async def do_rtfm(self, ctx, key, obj):
+    page_types = {
+      "appcommands": "https://dpy-appcommands.readthedocs.io/en/stable",
+      "python": "https://docs.python.org/3",
+      "discord": "https://discordpy.readthedocs.io/en/master",
+    }
+
+    if obj is None:
+      await ctx.send(page_types[key], ephemeral=True)
+      return
+
+    if not hasattr(self, "_rtfm_cache"):
+      await self.build_rtfm_lookup_table(page_types)
+
+    obj = re.sub(r"^(?:discord\.(?:ext\.)?)?(?:commands\.)?(.+)", r"\1", obj)
+    obj = re.sub(r"^(?:appcommands\.)?(.+)", r"\1", obj)
+
+    if key.startswith("discord"):
+      q = obj.lower()
+      for name in dir(discord.abc.Messageable):
+        if name[0] == "_":
+          continue
+
+        if q == name:
+          obj = f"abc.Messageable.{name}"
+          break
+
+    cache = list(self._rtfm_cache[key].items())
+
+    def transform(tup):
+      return tup[0]
+
+    matches = finder(obj, cache, key=lambda t: t[0], lazy=False)[:8]
+
+    e = discord.Embed(colour=0x00ffff)
+    if len(matches) == 0:
+      return await ctx.send("Could not find anything. Sorry.", ephemeral=True)
+
+    e.description = "\n".join(f"[`{key}`]({url})" for key, url in matches)
+    await ctx.send(embed=e,)
+
+  rtfm = appcommands.slashgroup(
+    name="rtfm",
+    description="Rtfm Commands",
+    guild_ids=[
+      821599275929567232,
+      783778325083848755,
+      731072681688039444,
+    ]
+  )
+
+  @rtfm.subcommand(name="appcommands", description="Search documentation of dpy-appcommands objects.")
+  async def rtfm_apc(self, ctx, *, query: Q = None):
+    await self.do_rtfm(ctx, "appcommands", query)
+
+  @rtfm.subcommand(name="discordpy", description="Search documentation of discord.py objects.")
+  async def rtfm_dsc(self, ctx, *, query: Q = None):
+    await self.do_rtfm(ctx, "discord", query)
+
+  @rtfm.subcommand(name="python", description="Search documentation of Python objects.")
+  async  def rtfm_py(self, ctx, *, query: Q = None):
+    await self.do_rtfm(ctx, "python", query,)
+
+
+@bot.usercommand(name="id")
+async def uid(ctx, user: discord.User):
+  await ctx.send(f"Id of {user.mention} is `{user.id}`", ephemeral=True)
+
+@bot.messagecommand(name="id")
+async def mid(ctx, message: discord.Message):
+  await ctx.send(f"Id of that message is `{message.id}`", ephemeral=True)
+
+bot.remove_command("help")
+
+def run():
+  try:
+    bot.load_extension("jishaku")
+    bot.add_cog(RTFMCog(bot))
+  except Exception as e:
+    print(e, file=sys.stderr)
+    traceback.print_exc()
+
+  bot.run(env.get("APP_BOT_TOKEN"))
